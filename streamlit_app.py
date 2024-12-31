6570272044cceb54260be13202e6c98323e61e17
iCode <kienchi261@gmail.com>
2024-12-31 08:59:36 +0000

    First edits to the basic app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 This is the app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). This will be very useful for sure!!!"
+)
+st.write(
+    "This shall be the start of something fruitful!"
 )
